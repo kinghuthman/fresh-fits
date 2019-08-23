beb7fcecca3ba6aba689ad0cb4bbd2b66f63df1c
Huthman <kinghuthman@gmail.com>
2019-08-22 20:56:50 -0400

    installed react-router-dom wrapped app with browser router to implement features from library used switch in app.js to have more control when accessing a path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component'
+
+const HatsPage = () => (
+  <div>
+    <h1> HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      {/**switch finds a match within the path and does not render anything else but that path  */}
+      <Switch>
+        <Route exact path = '/' component={Homepage} />
+        <Route path = '/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
