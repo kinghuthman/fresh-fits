861efab5a4cbf0c7a47b3f006519af169d1302cc
Huthman <kinghuthman@gmail.com>
2019-08-26 15:59:14 -0400

    collection-items & styling created a separate component for the individual collection items styled the items and preview

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-
+import CollectionItem from '../collection-item/collection-item.component';
 import './collection-preview.styles.scss';
 
 // 'collection-preview' contains entire collection for each directory
 // 'preview' filter just 4 items, items.map will list out each item in that directory
+// destructure off the id and the rest of the itemprops
 const CollectionPreview = ({title, items }) => (
     <div className = 'collection-preview'>
         <h1 classname = 'title'>{title.toUpperCase()}</h1>
         <div className = 'preview'>
             {
-                items.filter((item, idx) => idx < 4).map((item) => (
-                    <div key = {item.id}>{item.name}</div>
+                items.filter((item, idx) => idx < 4).map(({id, ...otherItemProps}) => (
+                    <CollectionItem key = {id} {...otherItemProps}/>
                 ))
             }
         </div>
