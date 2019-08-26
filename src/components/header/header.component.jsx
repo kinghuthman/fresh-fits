cc8fc3fd70bdea83e689cbdacf95ed6c59f4f8f6
Huthman <kinghuthman@gmail.com>
2019-08-26 17:20:48 -0400

    header created header component, visible on each page/route styled header header contains options/links to other pages within the app

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import {ReactComponent as Logo } from '../../assets/crown.svg';
+import './header.styles.scss';
+
+
+const Header = () => (
+    <div className = 'header'>
+        {/** logo links back to homepage */}
+        <Link classname = 'logo-container' to = '/'>
+            <Logo className = 'logo' />
+        </Link>
+        {/** special container for individual link options */}
+        <div className = 'options'>
+            <Link className = 'option' to = '/shop'>
+                SHOP
+            </Link>
+            <Link className = 'option' to = '/contact'>
+                CONTACT
+            </Link>
+        </div>
+    </div>
+)
+
+export default Header
